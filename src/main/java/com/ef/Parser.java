d186eee623ff898da355dbc6a368731f03173deb
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2018-09-01 17:31:24 -0300

    Adds parameter parsing to main class

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -1,9 +1,26 @@
 package com.ef;
 
+import com.ef.mapper.ArgsParser;
+import com.ef.mapper.GnuArgsParser;
+import com.ef.mapper.exceptions.BadArgumentsException;
+
 public class Parser {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
+		ArgsParser argsParser = new GnuArgsParser();
+		
+		try {
+			
+			argsParser.parse(args);
+		
+		} catch (BadArgumentsException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		
+		
+		System.exit(0);
 	}
 
 }
